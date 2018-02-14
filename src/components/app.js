d6d883015c1890567f7e458397840e43fd922ad5
Clark Cutler <clark@carbonfive.com>
2018-02-14 14:26:33 -0800

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import quickSelectImg from '../quick-select.png';
+
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>QuickSelect (React)</h2>
+        <p>
+          Implement a <code>QuickSelect</code> React component that accepts an array of data objects and displays it as
+          a table. Use <code>generateSampleData</code> from <code>src/services/data</code> for sample data.
+        </p>
+        <p>
+          <code>QuickSelect</code> should also render groups of checkboxes for each value present for each key in the
+          sample data. Rows matching any of the checked boxes should be highlighted.
+        </p>
+        <p>
+          <code>QuickSelect</code> should only generate these checkbox groups for keys that are passed in (as an array).
+          For example, <code>['category', 'color']</code>.
+        </p>
+        <p>
+          Here's an example image:
+        </p>
+        <img src={quickSelectImg} />
+      </div>
+    );
+  }
+}
+
+export default App;
