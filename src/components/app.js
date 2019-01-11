5b2be2cbfd58cea67926866305047a8d18879987
Paul Raythattha <paul@carbonfive.com>
2019-01-10 16:50:17 -0800

    Linting v1
      - Set up ESLint inspired by spraygun-react
        (https://github.com/carbonfive/spraygun-react/blob/master/.eslintrc.json)
    
      - Lint and fix the codebase.

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import quickSelectImg from '../quick-select.png';
+import quickSelectImg from "../quick-select.png";
 
-import './app.css';
+import "./app.css";
 
 class App extends Component {
   render() {
@@ -10,16 +10,20 @@ class App extends Component {
       <div>
         <h2>QuickSelect (React)</h2>
         <p>
-          Implement a <code>QuickSelect</code> React component that accepts an array of data objects and displays it as
-          a table. Use <code>generateSampleData</code> from <code>src/services/data</code> for sample data.
+          Implement a <code>QuickSelect</code> React component that accepts an
+          array of data objects and displays it as a table. Use{" "}
+          <code>generateSampleData</code> from <code>src/services/data</code>{" "}
+          for sample data.
         </p>
         <p>
-          <code>QuickSelect</code> should also render groups of checkboxes for each value present for each key in the
-          sample data. Rows matching any of the checked boxes should be highlighted.
+          <code>QuickSelect</code> should also render groups of checkboxes for
+          each value present for each key in the sample data. Rows matching any
+          of the checked boxes should be highlighted.
         </p>
         <p>
-          <code>QuickSelect</code> should only generate these checkbox groups for keys that are passed in (as an array).
-          For example, <code>['category', 'color']</code>.
+          <code>QuickSelect</code> should only generate these checkbox groups
+          for keys that are passed in (as an array). For example, 
+          <code>['category', 'color']</code>.
         </p>
         <p>Here is an example image:</p>
         <img src={quickSelectImg} alt="screenshot of sample solution" />
