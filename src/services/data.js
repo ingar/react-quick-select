a0d77c0b345b6fc8aae299a306b018170fe7e83c
Shannon Wells <shannonwells@users.noreply.github.com>
2021-03-15 17:37:50 -0700

    update packages and fix eslint errors (#1)

--- src/services/data.js
+++ src/services/data.js
@@ -1,10 +1,10 @@
 import { range, sample } from "lodash";
 
 export function generateSampleData() {
-  return range(10).map(index => ({
+  return range(10).map((index) => ({
     id: index,
     category: sample([null, "Category 1", "Category 2", "Category 3"]),
     color: sample(["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]),
-    size: sample(["Big", "Small"])
+    size: sample(["Big", "Small"]),
   }));
 }
